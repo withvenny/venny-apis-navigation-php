077371ff9e3ebc04f046ee682b671dc529bf2522
adolphusnolan <adolphusnolan@gmail.com>
2020-02-10 10:24:24 -0600

    PostgreSQL PHP: Create New Tables

--- app/v1/Connection.php
+++ app/v1/Connection.php
@@ -65,3 +65,64 @@ class Connection {
     }
  
 }
+
+class PostgreSQLCreateTable {
+ 
+    /**
+     * PDO object
+     * @var \PDO
+     */
+    private $pdo;
+ 
+    /**
+     * init the object with a \PDO object
+     * @param type $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+ 
+    /**
+     * create tables 
+     */
+    public function createTables() {
+        $sqlList = ['CREATE TABLE IF NOT EXISTS stocks (
+                        id serial PRIMARY KEY,
+                        symbol character varying(10) NOT NULL UNIQUE,
+                        company character varying(255) NOT NULL UNIQUE 
+                     );',
+            'CREATE TABLE IF NOT EXISTS stock_valuations (
+                        stock_id INTEGER NOT NULL,
+                        value_on date NOT NULL,
+                        price numeric(8,2) NOT NULL DEFAULT 0,
+                        PRIMARY KEY (stock_id, value_on),
+                        FOREIGN KEY (stock_id) REFERENCES stocks(id)
+                    );'];
+ 
+        // execute each sql statement to create new tables
+        foreach ($sqlList as $sql) {
+            $this->pdo->exec($sql);
+        }
+        
+        return $this;
+    }
+ 
+    /**
+     * return tables in the database
+     */
+    public function getTables() {
+        $stmt = $this->pdo->query("SELECT table_name 
+                                   FROM information_schema.tables 
+                                   WHERE table_schema= 'public' 
+                                        AND table_type='BASE TABLE'
+                                   ORDER BY table_name");
+        $tableList = [];
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $tableList[] = $row['table_name'];
+        }
+ 
+        return $tableList;
+    }
+}
+
+?>
