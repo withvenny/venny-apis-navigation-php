6b1f90b7800ae87e02ccb99f4a87fb0cd5425efd
adolphusnolan <adolphusnolan@gmail.com>
2020-02-10 09:05:43 -0600

    quick connection test

--- app/index.php
+++ app/index.php
@@ -1,14 +1,5 @@
 <?php
  
-    require 'vendor/autoload.php';
-    
-    use PostgreSQLTutorial\Connection as Connection;
-    
-    try {
-        Connection::get()->connect();
-        echo 'A connection to the PostgreSQL database sever has been established successfully.';
-    } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
+    echo "Hello, World!";
 
 ?>
