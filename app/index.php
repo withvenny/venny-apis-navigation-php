c598eb4dfa94afea3964ef7f826b0de3624f5e9b
adolphusnolan <adolphusnolan@gmail.com>
2020-02-10 10:29:13 -0600

    PostgreSQL PHP: Create New Tables v2
    I also would like to keep previously committed wins. Now building a comprehensive small app
    
    this keeps everything transparent for later review

--- app/index.php
+++ app/index.php
@@ -4,26 +4,34 @@
     
     use PostgreSQLTutorial\Connection as Connection;
     use PostgreSQLTutorial\PostgreSQLCreateTable as PostgreSQLCreateTable;
- 
-try {
+
     
-    // connect to the PostgreSQL database
-    $pdo = Connection::get()->connect();
-    
-    // 
-    $tableCreator = new PostgreSQLCreateTable($pdo);
-    
-    // create tables and query the table from the
-    // database
-    $tables = $tableCreator->createTables()
-                            ->getTables();
-    
-    foreach ($tables as $table){
-        echo $table . '<br>';
+    try {
+        Connection::get()->connect();
+        echo 'A connection to the PostgreSQL database sever has been established successfully.';
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
     }
+
+    try {
     
-} catch (\PDOException $e) {
-    echo $e->getMessage();
-}
+        // connect to the PostgreSQL database
+        $pdo = Connection::get()->connect();
+        
+        // 
+        $tableCreator = new PostgreSQLCreateTable($pdo);
+        
+        // create tables and query the table from the
+        // database
+        $tables = $tableCreator->createTables()
+                                ->getTables();
+        
+        foreach ($tables as $table){
+            echo $table . '<br>';
+        }
+        
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
+    }
 
 ?>
