d80b53071b7b6c564c0c884a018613f300444670
adolphusnolan <adolphusnolan@gmail.com>
2020-02-20 16:42:20 -0600

    refactoring for decoupling
    results disppeared for some reason

--- app/api.php
+++ app/api.php
@@ -7,22 +7,15 @@
     require '../vendor/autoload.php';
     require 'v1/functions.php';
 
-    //$token = new \Core\Token($this->pdo);
-    if(isset($request['token'])) {
-
-        $token = $this->token->validatedToken($request['token']);
+    //
+    switch ($_REQUEST['domain']) {
 
         //
-        switch ($_REQUEST['domain']) {
+        case 'persons': require 'endpoint-persons.php'; break;
 
-            //
-            case 'persons': require 'endpoint-identity-persons.php'; break;
-
-            //
-            default: header("Location: template-guest-hello.php");
-        
-        }
-
+        //
+        default: header("Location: template-guest-hello.php");
+    
     }
 
 ?>
