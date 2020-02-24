cce499c66d787b7cebbe9171f87201f48a784a8d
adolphusnolan <adolphusnolan@gmail.com>
2020-02-24 16:37:10 -0600

    Refactoring to excel
    test

--- app/v1/Identity.php
+++ app/v1/Identity.php
@@ -94,12 +94,12 @@
             if($statement->rowCount() > 0) {
 
                 //
-                $data = $statement->fetchObject(\PDO::FETCH_ASSOC));
+                $data = $statement->fetchObject(\PDO::FETCH_ASSOC);
                 
                 return $data;
         
             }
-            
+
         }
 
         /**
