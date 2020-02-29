cfc99b7a53763c4fbd55f82286dcf56b1bdc5583
adolphusnolan <adolphusnolan@gmail.com>
2020-02-28 18:50:49 -0600

    Strip

--- app/endpoint-experience-payment.php
+++ app/endpoint-experience-payment.php
@@ -11,4 +11,21 @@
   'receipt_email' => 'jenny.rosen@example.com',
 ]);
 
+$ch = \Stripe\Charge::retrieve(
+    "ch_1GHJRGEp9b2l1tcUAcotCgom",
+    ['api_key' => 'sk_test_mTRBViwNmba7buxiOehiBZu400QVrwfzzN'],
+  );
+  $ch->capture(); // Uses the same API Key.
+
+$ch = \Stripe\Charge::retrieve(
+    "ch_1GHJRGEp9b2l1tcUAcotCgom",
+    ['stripe_account' => 'acct_1DaaWEEp9b2l1tcU'],
+  );
+  $ch->capture(); // Uses the same account.
+
+  \Stripe\Charge::retrieve([
+    'id' => 'ch_1GHJRGEp9b2l1tcUAcotCgom',
+    'expand' => ['customer', 'invoice.subscription'],
+  ]);
+
 ?>
