fcc17bc9ea9091683e7e3a51d19d356ca30ea3d8
adolphusnolan <adolphusnolan@gmail.com>
2020-02-28 19:30:05 -0600

    Strip

--- app/endpoint-experience-payment.php
+++ app/endpoint-experience-payment.php
@@ -5,13 +5,27 @@
 
 \Stripe\Stripe::setApiKey('sk_test_mTRBViwNmba7buxiOehiBZu400QVrwfzzN');
 
-\Stripe\Token::create([
-  'card' => [
-    'number' => $_REQUEST['card'],
-    'exp_month' => 2,
-    'exp_year' => 2021,
-    'cvc' => '314',
-  ],
-]);
+switch ($_REQUEST['type']) {
+
+    case 'token':
+        \Stripe\Token::create([
+            'card' => [
+              'number' => $_REQUEST['card'],
+              'exp_month' => 2,
+              'exp_year' => 2021,
+              'cvc' => '314',
+            ],
+          ]);
+
+        break;
+
+        case 'retrievetoken':
+            \Stripe\Token::retrieve(
+                $_REQUEST['card']
+              );
+    
+            break;
+
+        }
 
 ?>
