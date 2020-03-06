48af3297cb32d3d4d507ecfb9d07d31697f21842
adolphusnolan <adolphusnolan@gmail.com>
2020-03-06 11:19:03 -0600

    /signin

--- app/endpoint-experience-signin.php
+++ app/endpoint-experience-signin.php
@@ -66,9 +66,9 @@
             $results['status'] = 200;
             $results['message'] = 'SUCCESSFULL';
             $results['data']=[];
-            $results['data'].=json_encode($person_details['data']);
-            $results['data'].=json_encode($user_details['data']);
-            $results['data'].=json_encode($profile_details['data']);
+            $results['data'].=$person_details['data'];
+            $results['data'].=$user_details['data'];
+            $results['data'].=$profile_details['data'];
             $results['log'] = [
                 'process' => $process_id = $token->process_id(),
                 'event' => $token->event_id($process_id)
