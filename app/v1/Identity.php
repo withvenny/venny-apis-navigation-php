de28cbff1419874682af2e34655e4127d9ae39db
adolphusnolan <adolphusnolan@gmail.com>
2020-03-06 05:01:05 -0600

    /signin

--- app/v1/Identity.php
+++ app/v1/Identity.php
@@ -631,7 +631,7 @@
 
                     $conditions.= " WHERE";
                     $conditions.= " person_id = :person ";
-                    $conditions.= " AND " . " user_authorize = crypt(".$request['authorize'].", user_authorize)";
+                    $conditions.= " AND " . " user_authorize = crypt(:authorize, user_authorize)";
                     $conditions.= " AND active = 1 ";
                     
                     $subset = " LIMIT 1";
