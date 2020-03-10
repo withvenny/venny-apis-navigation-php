5a28396bec79925da0936feed8ac65bf71879179
adolphusnolan <adolphusnolan@gmail.com>
2020-03-09 22:49:06 -0500

    /upload

--- app/index.php
+++ app/index.php
@@ -6,7 +6,7 @@
 
     // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
     $s3 = new Aws\S3\S3Client([
-        'version'  => '2006-03-01',
+        'version'  => 'latest',
         'region'   => 'us-east-2',
     ]);
 
